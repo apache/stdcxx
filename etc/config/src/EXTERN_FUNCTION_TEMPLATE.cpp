a9eaf32a2132f1ee67b63e2d81b882faa3c723f9
Martin Sebor <sebor@apache.org>
2005-09-20 00:31:27 +0000

    2005-09-19  Martin Sebor  <sebor@roguewave.com>
    	STDCXX-14 (part 3)
    	* extern_function_template.cpp: Renamed...
    	* extern_function_template_imp.cpp: ...to this.
    	* EXTERN_FUNCTION_TEMPLATE.cpp: Changed dependency on the above.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@290321 13f79535-47bb-0310-9956-ffa450edef68

--- etc/config/src/EXTERN_FUNCTION_TEMPLATE.cpp
+++ etc/config/src/EXTERN_FUNCTION_TEMPLATE.cpp
@@ -4,14 +4,14 @@
 #  include "config.h"
 #endif   // _RWSTD_USE_CONFIG
 
-// establish a dependency on extern_function_template.{cpp,o}
-#ifndef _RWSTD_NO_extern_function_template
+// establish a dependency on extern_function_template_imp.{cpp,o}
+#ifndef _RWSTD_NO_extern_function_template_imp
 
 // link the object file produced by compiling this file
 // with the object file below
-// LDOPTS = ./extern_function_template.o
+// LDOPTS = ./extern_function_template_imp.o
 
-#endif   // _RWSTD_NO_extern_function_template
+#endif   // _RWSTD_NO_extern_function_template_imp
 
 
 template <class T>
@@ -24,7 +24,7 @@ struct S
 template <class T>
 S<T> foobar (S<T> s)
 {
-    // foobar<int>() explicitly instantiated in extern_function_template.cpp
+    // foobar<int>() explicitly instantiated in extern_function_template_imp.cpp
     // is defined to set s.t to 1
     s.t = -1;
 
@@ -35,7 +35,7 @@ extern template S<int> foobar (S<int>);
 
 int main ()
 {
-    // S<int>::bar () is defined to return 1 in extern_function_template.o
+    // S<int>::bar () is defined to return 1 in extern_function_template_imp.o
 
     S<int> s;
 
@@ -45,7 +45,7 @@ int main ()
     res += foobar (s).t;
 
     // verify that the call resolves to the definition emitted
-    // by the explicit instantiation in extern_function_template.o
+    // by the explicit instantiation in extern_function_template_imp.o
     // and not the one here
     return !(1 == res);
 }
