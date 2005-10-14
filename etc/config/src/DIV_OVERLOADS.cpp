cf77967783f4fff54ba9eee9758c095113fbd2cb
Martin Sebor <sebor@apache.org>
2005-10-14 19:02:01 +0000

    2005-10-14  Martin Sebor  <sebor@roguewave.com>
    	STDCXX-46
    	* DIV_OVERLOADS.cpp: New config test to determine the presence
    	of the C++ overloads of the div() function in <stdlib.h> and
    	the rest of the C standard library headers.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@321182 13f79535-47bb-0310-9956-ffa450edef68

---
+++ etc/config/src/DIV_OVERLOADS.cpp
@@ -0,0 +1,62 @@
+// checking for overloads of div()
+
+// determine whether the div() function (including
+// any overloads) is defined in the usual libc headers
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#if defined (_RWSTD_USE_CONFIG)
+#  include "config.h"
+#endif   // _RWSTD_USE_CONFIG
+
+#ifndef _RWSTD_NO_LONG_LONG
+#  if defined _MSC_VER
+#    define LONG_LONG   __int64
+#  else
+#    define LONG_LONG   long long
+#  endif
+#endif   // _RWSTD_NO_LONG_LONG
+
+
+int template_used;
+
+template <class T>
+ldiv_t div (T, T)
+{
+    ++template_used;
+
+    return ldiv_t ();
+}
+
+template <class T>
+void check_div (T t, const char *tname)
+{
+    template_used = 0;
+
+    div (t, t);
+
+    if (0 == template_used)
+        printf ("%s", "// ");
+
+    printf ("#define _RWSTD_NO_DIV_%s\n", tname);
+}
+
+int main ()
+{
+#if !defined (_RWSTD_USE_CONFIG)
+
+    printf ("/**/\n#undef _RWSTD_DIV_OVERLOADS\n");
+
+#endif   // _RWSTD_USE_CONFIG
+
+    check_div ((int)0, "INT");
+    check_div ((long)0, "LONG");
+
+#if defined (LONG_LONG)
+    check_div ((LONG_LONG)0, "LLONG");
+#endif   // LONG_LONG
+
+    return 0;
+}
