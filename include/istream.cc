dc30760b0d053b9875212a896a111222a94fd46f
Martin Sebor <sebor@apache.org>
2005-11-16 23:37:58 +0000

    2005-11-16  Martin Sebor  <sebor@roguewave.com>
    	* istream.cc: Removed HP copyright notice (HP had nothing to do with
    	this file).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@345150 13f79535-47bb-0310-9956-ffa450edef68

--- include/istream.cc
+++ include/istream.cc
@@ -6,19 +6,6 @@
  *
  ***************************************************************************
  *
- * Copyright (c) 1994
- * Hewlett-Packard Company
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Hewlett-Packard Company makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
- *
- ***************************************************************************
- *
  * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
  * Software division. Licensed under the Apache License, Version 2.0 (the
  * "License");  you may  not use this file except  in compliance with the
