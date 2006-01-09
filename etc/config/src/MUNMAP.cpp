83dd8875f17a973fff7d13276adb0664460bc1c5
Martin Sebor <sebor@apache.org>
2006-01-09 19:48:30 +0000

    2006-01-09  Martin Sebor  <sebor@roguewave.com>
    	STDCXX-107
    	* MUNMAP.cpp (<sys/types.h>): Included to prevent compilation errors
    	in <sys/mman.h> on BSD.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@367374 13f79535-47bb-0310-9956-ffa450edef68

--- etc/config/src/MUNMAP.cpp
+++ etc/config/src/MUNMAP.cpp
@@ -4,7 +4,10 @@
 #  include "config.h"
 #endif   // _RWSTD_USE_CONFIG
 
-#include <sys/mman.h>
+// include <sys/types.h> first in case mman.h depends
+// on it (such as the one on Darwin -- see STDCXX-107)
+#include <sys/types.h>   // for size_t
+#include <sys/mman.h>    // for munmap()
 
 extern "C" {
 
@@ -14,13 +17,14 @@ typedef int (f_i_cpui)(char*, size_t);
 }
 
 
-const char* foo (f_i_vpui*) { return "void*"; }
-const char* foo (f_i_cpui*) { return "char*"; }
+const char* munmap_arg2_type_imp (f_i_vpui*) { return "void*"; }
+const char* munmap_arg2_type_imp (f_i_cpui*) { return "char*"; }
 
-
-const char* bar ()
+// determine whether the type of the second argument
+// to munmap() is void* or char*
+const char* get_munmap_arg2_type ()
 {
-    return foo (&munmap);
+    return munmap_arg2_type_imp (&munmap);
 }
 
 
@@ -31,14 +35,14 @@ const char* bar ()
 
 int main ()
 {
-    const char *argtype = bar ();
-
 #if !defined (_RWSTD_USE_CONFIG)
 
     printf ("/**/\n#undef _RWSTD_MUNMAP\n");
 
 #endif   // _RWSTD_USE_CONFIG
     
+    const char *argtype = get_munmap_arg2_type ();
+
     printf ("#define _RWSTD_MUNMAP_ARG1_T %s\n", argtype);
 
     return 0;
