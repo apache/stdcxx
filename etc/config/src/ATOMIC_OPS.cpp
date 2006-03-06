923d43045691972d31c12a56c714213dd8c515cb
Martin Sebor <sebor@apache.org>
2006-03-06 21:02:51 +0000

    2006-03-06  Martin Sebor  <sebor@roguewave.com>
    	* ATOMIC_OPS.cpp: Added/changed comments.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@383667 13f79535-47bb-0310-9956-ffa450edef68

--- etc/config/src/ATOMIC_OPS.cpp
+++ etc/config/src/ATOMIC_OPS.cpp
@@ -27,16 +27,20 @@ int main ()
 {
 #if !defined (_RWSTD_USE_CONFIG)
 
-    printf (":\n#undef _RWSTD_ATOMIC_OPS\n");
+    printf ("/**/\n#undef _RWSTD_ATOMIC_OPS\n");
 
 #endif   // _RWSTD_USE_CONFIG
+
+    // determine the argument type of InterlockedIncrement()
+    // (the type changes from long* to volatile long* depending
+    // on the version/patch of MSVC)
 
     printf ("#define _RWSTD_INTERLOCKED_T %s\n", foo (InterlockedIncrement));
 
     return 0;
 }
 
-#else
+#else   // not Windows
 
 #include <stdio.h>
 
@@ -44,11 +48,11 @@ int main ()
 {
 #if !defined (_RWSTD_USE_CONFIG)
 
-    printf (":\n#undef _RWSTD_ATOMIC_OPS\n");
+    printf ("/**/\n#undef _RWSTD_ATOMIC_OPS\n");
 
 #endif   // _RWSTD_USE_CONFIG
 
     return 0;
 }
 
-#endif
+#endif   // Windows
