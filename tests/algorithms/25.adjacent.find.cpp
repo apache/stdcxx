f3cd0fd98bf0a23fb57a134e4823b54448f6f578
Martin Sebor <sebor@apache.org>
2006-03-16 22:12:10 +0000

    2006-03-16  Martin Sebor  <sebor@roguewave.com>
    	* 25.adjacent.find.cpp (test_adjacent_find): Corrected paramno values
    	to account for the change in the handling of conditionals in r371638.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@386470 13f79535-47bb-0310-9956-ffa450edef68

--- tests/algorithms/25.adjacent.find.cpp
+++ tests/algorithms/25.adjacent.find.cpp
@@ -1,21 +1,27 @@
 /***************************************************************************
  *
- * adjacent_find.cpp - test exercising 25.1.5 [lib.alg.adjacent.find]
+ * 25.adjacent.find.cpp - test exercising lib.alg.adjacent.find
  *
  * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,
+ * as applicable.
+ *
+ * Copyright 2004-2006 Rogue Wave Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  **************************************************************************/
 
@@ -224,9 +230,9 @@ void test_adjacent_find (const char* predname)
 {
     rw_info (0, 0, 0, 
              "template <class %s%{?}, class %s%{;}> "
-             "%1$s std::adjacent_find (%1$s, %1$s%{?}, %2$s%{;})",
-             "ForwardIterator",
-             0 != predname, "BinaryPredicate", 0 != predname);
+             "%1$s std::adjacent_find (%1$s, %1$s%{?}, %3$s%{;})",
+             "ForwardIterator", 0 != predname, "BinaryPredicate",
+             0 != predname);
 
     if (rw_opt_no_fwd_iter) {
         rw_note (0, __FILE__, __LINE__, "ForwardIterator test disabled");
