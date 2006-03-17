0accb66947d39983881198909b9a745fe7279190
Martin Sebor <sebor@apache.org>
2006-03-17 21:42:20 +0000

    2006-03-17  Martin Sebor  <sebor@roguewave.com>
    	* ti_filebuf.cpp (<stdio.h>): Included before <fstream> to get
    	the extended member functions of basic_filebuf that take FILE*
    	as an argument defined and to prevent unsats from programs that
    	do the same thing when extern template is enabled.
    	* ti_filebuf.cpp: Ditto.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@386729 13f79535-47bb-0310-9956-ffa450edef68

--- src/ti_wfilebuf.cpp
+++ src/ti_wfilebuf.cpp
@@ -3,20 +3,26 @@
  * ti_wfilebuf.cpp - explicit instantiation directives
  *                   for basic_filebuf<wchar_t>
  *
- * $Id: //stdlib/dev/source/stdlib/ti_wfilebuf.cpp#5 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,
+ * as applicable.
+ *
+ * Copyright 1994-2006 Rogue Wave Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  **************************************************************************/
 
@@ -36,4 +42,7 @@
 #  pragma warning (disable: 4661)
 #endif   // _MSC_VER
 
+// include <cstdio> to define the extended member functions that
+// take a FILE* argument
+#include <cstdio>
 #include <fstream>
