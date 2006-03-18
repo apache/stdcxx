b0a280c7b665a3f73f75ff4e45684f5ba6be6a37
Martin Sebor <sebor@apache.org>
2006-03-18 22:59:00 +0000

    2006-03-18  Martin Sebor  <sebor@roguewave.com>
    	* localedef.h: Renamed...
    	* rw_locale.h: ...to this to prevent clashing with the HP-UX
    	system header with the same name.
    	* localedef.cpp: Renamed...
    	* locale.cpp: ...to this to (pretty closely) match header name.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@386898 13f79535-47bb-0310-9956-ffa450edef68

--- tests/include/rw_locale.h
+++ tests/include/rw_locale.h
@@ -6,21 +6,27 @@
  *
  ************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,
+ * as applicable.
  *
+ * Copyright 2001-2006 Rogue Wave Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  **************************************************************************/
 
-#ifndef RW_LOCALEDEF_H_INCLUDED
-#define RW_LOCALEDEF_H_INCLUDED
+#ifndef RW_LOCALE_H_INCLUDED
+#define RW_LOCALE_H_INCLUDED
 
 
 #include <testdefs.h>
@@ -58,4 +64,4 @@ _TEST_EXPORT const char*
 rw_localedef (const char*, const char*, const char*, const char*);
 
 
-#endif   // RW_LOCALEDEF_H_INCLUDED
+#endif   // RW_LOCALE_H_INCLUDED
--- tests/src/locale.cpp
+++ tests/src/locale.cpp
@@ -1,21 +1,27 @@
 /************************************************************************
  *
- * localedef.cpp - definitions of locale helpers
+ * locale.cpp - definitions of locale helpers
  *
- * $Id:$
+ * $Id$
  *
  ************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,
+ * as applicable.
+ *
+ * Copyright 2001-2006 Rogue Wave Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  **************************************************************************/
 
@@ -23,14 +29,7 @@
 #define _RWSTD_TEST_SRC
 
 
-#ifdef _RWSTD_USE_CONFIG
-   // FIXME: use one style of #include paths consistently throughout
-   // the whole test suite (this form prevents #including the HP-UX
-   // /usr/include/localedef.h header)
-#  include "../include/localedef.h"
-#else
-#  include <localedef.h>
-#endif
+#include <rw_locale.h>
 
 #include <environ.h>    // for rw_putenv()
 #include <file.h>       // for SHELL_RM_RF, rw_tmpnam
