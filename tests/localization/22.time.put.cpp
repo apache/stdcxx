a8ebfc01ec134a848e6d8aab07f05cff7a763056
Martin Sebor <sebor@apache.org>
2006-03-18 23:09:38 +0000

    2006-03-18  Martin Sebor  <sebor@roguewave.com>
    	* 22.time.put.cpp (<rw_locale.h>): Included instead of <localedef.h>.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@386902 13f79535-47bb-0310-9956-ffa450edef68

--- tests/localization/22.time.put.cpp
+++ tests/localization/22.time.put.cpp
@@ -6,16 +6,22 @@
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,
+ * as applicable.
+ *
+ * Copyright 2002-2006 Rogue Wave Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  **************************************************************************/
 
@@ -32,7 +38,7 @@
 #include <driver.h>      // for rw_test(), ...
 #include <environ.h>     // for rw_putenv()
 #include <file.h>        // for pcs_write(), ...
-#include <localedef.h>   // for rw_localedef(), ...
+#include <rw_locale.h>   // for rw_localedef(), ...
 #include <valcmp.h>      // for rw_strncmp()
 
 
