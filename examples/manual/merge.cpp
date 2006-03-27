643519ced69dd3559b98efa5a07519c39de61432
Martin Sebor <sebor@apache.org>
2006-03-27 23:24:56 +0000

    2006-03-27  Martin Sebor  <sebor@roguewave.com>
    	* merge.cpp: Used '\n' instead of endl.
    	* merge.out: Added terminating newline.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@389321 13f79535-47bb-0310-9956-ffa450edef68

--- examples/manual/merge.cpp
+++ examples/manual/merge.cpp
@@ -1,32 +1,29 @@
 /**************************************************************************
  *
- * merge.cpp - Example program of merging sequences. 
+ * merge.cpp - Example program demonstrating the merge algorithms.
  *
- * $Id: //stdlib/dev/examples/stdlib/manual/merge.cpp#13 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,
+ * as applicable.
+ *
+ * Copyright 1994-2006 Rogue Wave Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  **************************************************************************/
-
-#include <rw/_config.h>
-
-#if defined (__IBMCPP__) && !defined (_RWSTD_NO_IMPLICIT_INCLUSION)
-// Disable implicit inclusion to work around 
-// a limitation in IBM's VisualAge 5.0.2.0 (see PR#26959) 
-
-#  define _RWSTD_NO_IMPLICIT_INCLUSION 
-#endif
 
 #include <algorithm>    // advance, copy, inplace_merge, merge
 #include <functional>   // less
@@ -65,7 +62,7 @@ int main ()
     std::merge (v1.begin (), v1.end (),
                 v2.begin (), v2.end (), v4.begin (), std::less<int>());
 
-    // In place merge v5.
+    // Merge v5 in place.
     Vector::iterator mid = v5.begin ();
     std::advance (mid, 4);   // equivalent to mid += 4 but more generic
     std::inplace_merge (v5.begin (), mid, v5.end ());
@@ -81,25 +78,26 @@ int main ()
 
     // Copy all to cout.
     std::ostream_iterator<int, char, std::char_traits<char> >
-        out (std::cout," ");
+        out (std::cout, " ");
 
     std::copy (v1.begin (), v1.end (), out);
-    std::cout << std::endl;
-    std::copy (v2.begin(), v2.end (), out);
-    std::cout << std::endl;
+    std::cout << '\n';
+    std::copy (v2.begin (), v2.end (), out);
+    std::cout << '\n';
     std::copy (v3.begin (), v3.end (), out);
-    std::cout << std::endl;
+    std::cout << '\n';
     std::copy (v4.begin (), v4.end (), out);
-    std::cout << std::endl;
+    std::cout << '\n';
     std::copy (v5.begin (), v5.end (), out);
-    std::cout << std::endl;
-    std::copy (v6.begin (),v6.end (), out);
-    std::cout << std::endl;
+    std::cout << '\n';
+    std::copy (v6.begin (), v6.end (), out);
+    std::cout << '\n';
     std::copy (v7.begin (), v7.end (), out);
-    std::cout << std::endl;
+    std::cout << '\n';
 
     // Merge v1 and v2 to cout.
     std::merge (v1.begin (), v1.end (), v2.begin (), v2.end (), out);
+    std::cout << '\n';
 
     return 0;
 }
