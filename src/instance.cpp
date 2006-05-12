0170aae36b9f7d8972477c0e478d3781c542746f
Martin Sebor <sebor@apache.org>
2006-05-12 22:40:27 +0000

    2006-05-12  Martin Sebor  <sebor@roguewave.com>
    	* instance.cpp (__nullref): Increased the size of the array to make
    	big enough to accommodate std::wstring times two with some extra
    	space to spare.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@405913 13f79535-47bb-0310-9956-ffa450edef68

--- src/instance.cpp
+++ src/instance.cpp
@@ -2,20 +2,26 @@
  *
  * instance.cpp - Instantiations of the C++ Standard Library templates.
  *
- * $Id: //stdlib/dev/source/stdlib/instance.cpp#50 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,
+ * as applicable.
+ *
+ * Copyright 1994-2006 Rogue Wave Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  **************************************************************************/
 
@@ -32,11 +38,26 @@
 
 _RWSTD_NAMESPACE (__rw) {
 
-typedef __string_ref<char, _STD::char_traits<char>, _STD::allocator<char> >
-    StringRef;
+#ifndef _RWSTD_NO_WCHAR_T
 
-// this is wrong -- it won't work at all for other (non-char) specializations
-_RWSTD_EXPORT unsigned long __nullref [sizeof (StringRef) / 4 + 2];
+typedef _STD::char_traits<wchar_t> Traits;
+typedef _STD::allocator<wchar_t>   Allocator;
+
+typedef __string_ref<wchar_t, Traits, Allocator> StringRef;
+
+#else   // if defined (_RWSTD_NO_WCHAR_T)
+
+typedef _STD::char_traits<char> Traits;
+typedef _STD::allocator<char>   Allocator;
+
+typedef __string_ref<char, Traits, Allocator> StringRef;
+
+#endif   // _RWSTD_NO_WCHAR_T
+
+// FIXME: this won't work at all for other (non-char) specializations
+// try to allocate enough space for std::wstring plus some overhead
+_RWSTD_EXPORT unsigned long
+__nullref [4 * sizeof (StringRef) / sizeof (long) + 2];
 
 }   // namespace __rw
 
