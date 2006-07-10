0ddc25908928b13b653f717907abc31b5e6a7584
Martin Sebor <sebor@apache.org>
2006-07-10 23:17:31 +0000

    2006-07-10  Martin Sebor  <sebor@roguewave.com>
    	STDCXX-255
    	* locale.cpp (initialize): Avoided trying to validate and diagnose
    	null pointers.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@420657 13f79535-47bb-0310-9956-ffa450edef68

--- util/locale.cpp
+++ util/locale.cpp
@@ -2,20 +2,26 @@
  *
  * locale.cpp
  *
- * $Id: //stdlib/dev/source/stdlib/util/locale.cpp#110 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,
+ * as applicable.
+ *
+ * Copyright 2001-2006 Rogue Wave Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  **************************************************************************/
 
@@ -227,7 +233,7 @@ initialize (const std::string &loc_path_root,
         initialize_struct (loc_path_root, all_val, lang_val,
                            cat_name, codeset_name);
 
-    if (0 == validate (ptr, sizeof (FacetData)))
+    if (ptr && 0 == validate (ptr, sizeof (FacetData)))
         return static_cast<const FacetData*>(ptr);
 
     return 0;
