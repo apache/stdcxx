14b512dcd684fe1d7419864c680505ec37c2ecc9
Martin Sebor <sebor@apache.org>
2006-07-23 23:50:25 +0000

    2006-07-23  Martin Sebor  <sebor@roguewave.com>
    	* 21.string.append.cpp (AppendRangeOverload): Qualified references
    	to dependent base member function begin() with this-> to allow lookup
    	to find them.
    	* 21.string.assign.cpp (AssignRangeOverload): Same.
    	* 21.string.cons.cpp (ConsRangeOverload): Same.
    	* 21.string.insert.cpp (InsertRangeOverload): Same.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@424860 13f79535-47bb-0310-9956-ffa450edef68

--- tests/strings/21.string.assign.cpp
+++ tests/strings/21.string.assign.cpp
@@ -6,22 +6,21 @@
  *
  ***************************************************************************
  *
- * Copyright 2006 The Apache Software Foundation or its licensors,
- * as applicable.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
  *
- * Copyright 2006 Rogue Wave Software.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
  *
  **************************************************************************/
 
@@ -493,7 +492,7 @@ struct AssignRangeOverload: RangeBase<String>
             // exercise self-referential modification (i.e., replacing
             // a range of elements with a subrange of its own elements)
 
-            const Iterator first (begin (str, (Iterator*)0) + off);
+            const Iterator first (this->begin (str, (Iterator*)0) + off);
             const Iterator last (first + ext);
 
             return str.assign (first, last);
@@ -501,7 +500,7 @@ struct AssignRangeOverload: RangeBase<String>
 
         String str_arg (tdata.arg_, tdata.arglen_);
 
-        const Iterator first (begin (str_arg, (Iterator*)0) + off);
+        const Iterator first (this->begin (str_arg, (Iterator*)0) + off);
         const Iterator last (first + ext);
 
         return str.assign (first, last);
--- tests/strings/21.string.insert.cpp
+++ tests/strings/21.string.insert.cpp
@@ -6,22 +6,21 @@
  *
  ***************************************************************************
  *
- * Copyright 2006 The Apache Software Foundation or its licensors,
- * as applicable.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
  *
- * Copyright 2006 Rogue Wave Software.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
  *
  **************************************************************************/
 
@@ -616,7 +615,7 @@ struct InsertRangeOverload: RangeBase<String>
             // exercise self-referential modification (i.e., replacing
             // a range of elements with a subrange of its own elements)
 
-            const Iterator first2 (begin (str, (Iterator*)0) + off);
+            const Iterator first2 (this->begin (str, (Iterator*)0) + off);
             const Iterator last2 (first2 + ext);
 
             str.insert (first1, first2, last2);
@@ -625,7 +624,7 @@ struct InsertRangeOverload: RangeBase<String>
 
         String str_arg (tdata.arg_, tdata.arglen_);
 
-        const Iterator first2 (begin (str_arg, (Iterator*)0) + off);
+        const Iterator first2 (this->begin (str_arg, (Iterator*)0) + off);
         const Iterator last2 (first2 + ext);
 
         str.insert (first1, first2, last2);
