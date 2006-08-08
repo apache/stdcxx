b4f33a028d10b9fb47ed98867d235833a50005a1
Martin Sebor <sebor@apache.org>
2006-08-08 16:27:36 +0000

    2006-08-08  Martin Sebor  <sebor@roguewave.com>
    	* limits.cpp: Added output for numeric_limits<int> and changed
    	format to make it more readable.
    	* limits.out: Same.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@429712 13f79535-47bb-0310-9956-ffa450edef68

--- examples/manual/limits.cpp
+++ examples/manual/limits.cpp
@@ -3,20 +3,27 @@
  * limits.cpp - Example program of numeric limits class used for 
  *              representing information about scalar types.
  *
- * $Id: //stdlib/dev/examples/stdlib/manual/limits.cpp#12 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 1998-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -29,56 +36,72 @@
 template <class T>
 void print_limits (std::ostream &strm, const char *tname, T)
 {
-#define PRINT_MEMBER(member)                                       \
-    strm << "std::numeric_limits<" << tname << ">::" #member " = " \
-         << std::numeric_limits<T>::member << '\n';
+#define PRINT_MEMBER(type, member)                     \
+    strm << "    static " << type << " " #member " = "            \
+         << std::numeric_limits<T>::member << ";\n"
 
-    PRINT_MEMBER (is_specialized);
+    strm << "struct std::numeric_limits<" << tname << "> {\n";
 
-    PRINT_MEMBER (min ());
-    PRINT_MEMBER (max ());
+    PRINT_MEMBER ("const bool", is_specialized);
 
-    PRINT_MEMBER (digits);
-    PRINT_MEMBER (digits10);
+    PRINT_MEMBER (tname, min ());
+    PRINT_MEMBER (tname, max ());
 
-    PRINT_MEMBER (is_signed);
-    PRINT_MEMBER (is_integer);
-    PRINT_MEMBER (is_exact);
-    PRINT_MEMBER (radix);
+    PRINT_MEMBER ("const int", digits);
+    PRINT_MEMBER ("const int", digits10);
 
-    PRINT_MEMBER (epsilon ());
-    PRINT_MEMBER (round_error ());
+    PRINT_MEMBER ("const bool", is_signed);
+    PRINT_MEMBER ("const bool", is_integer);
+    PRINT_MEMBER ("const bool", is_exact);
+    PRINT_MEMBER ("const int", radix);
 
-    PRINT_MEMBER (min_exponent);
-    PRINT_MEMBER (min_exponent10);
-    PRINT_MEMBER (max_exponent);
-    PRINT_MEMBER (max_exponent10);
+    PRINT_MEMBER (tname, epsilon ());
+    PRINT_MEMBER ("int", round_error ());
 
-    PRINT_MEMBER (has_infinity);
-    PRINT_MEMBER (has_quiet_NaN);
-    PRINT_MEMBER (has_signaling_NaN);
-    PRINT_MEMBER (has_denorm);
+    PRINT_MEMBER ("const int", min_exponent);
+    PRINT_MEMBER ("const int", min_exponent10);
+    PRINT_MEMBER ("const int", max_exponent);
+    PRINT_MEMBER ("const int", max_exponent10);
 
-    PRINT_MEMBER (has_denorm_loss);
+    PRINT_MEMBER ("const bool", has_infinity);
+    PRINT_MEMBER ("const bool", has_quiet_NaN);
+    PRINT_MEMBER ("const bool", has_signaling_NaN);
+    PRINT_MEMBER ("const bool", has_denorm);
 
-    PRINT_MEMBER (infinity ());
-    PRINT_MEMBER (quiet_NaN ());
-    PRINT_MEMBER (signaling_NaN ());
-    PRINT_MEMBER (denorm_min ());
+    PRINT_MEMBER ("const bool", has_denorm_loss);
 
-    PRINT_MEMBER (is_iec559);
-    PRINT_MEMBER (is_bounded);
-    PRINT_MEMBER (is_modulo);
+    PRINT_MEMBER (tname, infinity ());
+    PRINT_MEMBER (tname, quiet_NaN ());
+    PRINT_MEMBER (tname, signaling_NaN ());
+    PRINT_MEMBER (tname, denorm_min ());
 
-    PRINT_MEMBER (traps);
-    PRINT_MEMBER (tinyness_before);
+    PRINT_MEMBER ("const bool", is_iec559);
+    PRINT_MEMBER ("const bool", is_bounded);
+    PRINT_MEMBER ("const bool", is_modulo);
 
-    PRINT_MEMBER (round_style);
+    PRINT_MEMBER ("const bool", traps);
+    PRINT_MEMBER ("const bool", tinyness_before);
+
+    PRINT_MEMBER ("const int", round_style);
+
+    strm << "};\n";
 }
+
 
 int main ()
 {
-    print_limits<double>(std::cout, "double", 0.0);
+#define PRINT_LIMITS(T) print_limits (std::cout, #T, T ())
+
+    // print bool values as "false" and "true"
+    std::cout.setf (std::cout.boolalpha);
+
+    // print the numeric limits for an integer type
+    PRINT_LIMITS (int);
+
+    std::cout << '\n';
+
+    // print the numeric limits for a floating point type
+    PRINT_LIMITS (double);
 
     return 0;
 }
