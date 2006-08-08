296040e4506c39907285b2dd9fc75fea5e0a23d9
Martin Sebor <sebor@apache.org>
2006-08-08 17:45:04 +0000

    2006-08-08  Martin Sebor  <sebor@roguewave.com>
    	* ostream1.cpp: Hardcoded 64-bit output for platforms with
    	no 64-bit integer type to prevent comparison failures while
    	testing.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@429750 13f79535-47bb-0310-9956-ffa450edef68

--- examples/manual/ostream1.cpp
+++ examples/manual/ostream1.cpp
@@ -6,16 +6,23 @@
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 1998-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -134,6 +141,21 @@ int main ()
         << std::dec << std::setw (20) << u64_max << ' '
         << std::oct << std::setw (23) << u64_max << ' '
         << std::hex << u64_max << '\n';
+
+#else   // if defined (NO_INT64)
+
+    // hardcoded for platforms with no 64-bit type
+    out << "int64_t min  = -9223372036854775808 "
+        << "01000000000000000000000 "
+        << "0x8000000000000000\n";
+
+    out << "int64_t max  = +9223372036854775807  "
+        << "0777777777777777777777 "
+        << "0x7fffffffffffffff\n";
+
+    out << "uint64_t max = 18446744073709551615 "
+        << "01777777777777777777777 "
+        << "0xffffffffffffffff\n";
 
 #endif   // NO_INT64
 
