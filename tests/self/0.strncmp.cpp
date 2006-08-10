1a7aa158cfde9966e6e9b96d5af96cfb90a23dbb
Martin Sebor <sebor@apache.org>
2006-08-10 21:33:38 +0000

    2006-08-10  Martin Sebor  <sebor@roguewave.com>
    	* 0.strncmp.cpp (TEST): Avoided unnecessarily casting away constness
    	in order to silence gcc warning: cast from `const char*' to `void*'
    	discards qualifiers from pointer target type.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@430542 13f79535-47bb-0310-9956-ffa450edef68

--- tests/self/0.strncmp.cpp
+++ tests/self/0.strncmp.cpp
@@ -6,22 +6,23 @@
  *
  ************************************************************************
  *
- * Copyright 2005-2006 The Apache Software Foundation or its licensors,
- * as applicable.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
  *
- * Copyright 2004-2006 Rogue Wave Software.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 2004-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -65,8 +66,9 @@ run_test (int, char*[])
         if (expect != result) {                                           \
            exit_status = 2;                                               \
            fprintf (stderr, "line %d: rw_strncmp(%p, %p, %u, %d) == %d, " \
-                            "got %d\n", __LINE__, (void*)s1, (void*)s2,   \
-                            unsigned (nc), fl, expect, result);           \
+                            "got %d\n", __LINE__, (const void*)s1,        \
+                            (const void*)s2, unsigned (nc), fl, expect,   \
+                            result);                                      \
         }                                                                 \
     } while (0)
 
