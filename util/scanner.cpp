80d92091a4992b8e9bf85f71772b4c90f8e41aa5
Martin Sebor <sebor@apache.org>
2006-08-16 16:09:49 +0000

    2006-08-16 Andrew Black <ablack@roguewave.com>
    	* scanner.cpp (Scanner::convert_escape): Corrected order of arguments
    	to an issue_diag call, added missing newline.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@431937 13f79535-47bb-0310-9956-ffa450edef68

--- util/scanner.cpp
+++ util/scanner.cpp
@@ -2,20 +2,27 @@
  *
  * scanner.cpp
  *
- * $Id: //stdlib/dev/source/stdlib/util/scanner.cpp#59 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 2001-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -734,8 +741,8 @@ convert_escape (const char* esc, const char **pend /* = 0 */) const
 
     if (val < 0 || val > long (UCHAR_MAX))
         issue_diag (E_INVAL, true, 0,
-                    "%s value in the range [0, %lu) expected: %s",
-                    basename, esc, long (UCHAR_MAX));
+                    "%s value in the range [0, %lu) expected: %s\n",
+                    basename, long (UCHAR_MAX), esc);
 
     typedef unsigned char UChar;
 
