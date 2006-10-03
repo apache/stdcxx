b2510ac2a27cb10be1ffe5009d0ecc306f73748c
Farid Zaripov <faridz@apache.org>
2006-10-03 14:28:24 +0000

    2006-10-03 Farid Zaripov <faridz@kyiv.vdiweb.com>
    	* _defs.h: _RWBUILD_std replaced to _RWSTD_LIB_SRC
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@452509 13f79535-47bb-0310-9956-ffa450edef68

--- include/rw/_defs.h
+++ include/rw/_defs.h
@@ -9,23 +9,24 @@
  *
  ***************************************************************************
  *
- * Copyright 2005-2006 The Apache Software Foundation or its licensors,
- * as applicable.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
  *
  * Copyright 1994-2006 Rogue Wave Software.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
  **************************************************************************/
 
 #ifndef _RWSTD_RW_DEFS_H_INCLUDED
@@ -458,11 +459,11 @@
 #if (defined (_WIN32) || defined (_WIN64)) && \
     (defined (RWDLL)  || defined (_RWSHARED))
 
-#  ifdef _RWBUILD_std
+#  ifdef _RWSTD_LIB_SRC
 #    define _RWSTD_EXPORT            __declspec (dllexport)
 #  else
 #    define _RWSTD_EXPORT            __declspec (dllimport)
-#  endif   // _RWBUILD_std
+#  endif   // _RWSTD_LIB_SRC
 
 #  if _MSC_VER >= 1300
 #    define _RWSTD_CLASS_EXPORT    _RWSTD_EXPORT
