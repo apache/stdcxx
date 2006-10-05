a0c02376351daac8d5942577a9447b12b81b8e03
Martin Sebor <sebor@apache.org>
2006-10-05 15:29:29 +0000

    2006-10-05  Martin Sebor  <sebor@roguewave.com>
    	* time_put.cpp (__rw_get_time_put_data): Added braces around
    	the initializer of a local array to silence a gcc warning.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@453270 13f79535-47bb-0310-9956-ffa450edef68

--- src/time_put.cpp
+++ src/time_put.cpp
@@ -6,22 +6,23 @@
  *
  ***************************************************************************
  *
- * Copyright 2005-2006 The Apache Software Foundation or its licensors,
- * as applicable.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
  *
- * Copyright 2001-2006 Rogue Wave Software.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 2001-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -2097,7 +2098,7 @@ __rw_get_time_put_data (__rw_time_put_data &tpd,
         wchar_t wide_data [1];
 #endif   // _RWSTD_NO_WCHAR_T
         char data [4];
-    } null_fmt = { 0 };
+    } null_fmt = { { 0 } };
 
     const __rw_time_t *ptime =
         _RWSTD_STATIC_CAST (const __rw_time_t*, facet->_C_data ());
