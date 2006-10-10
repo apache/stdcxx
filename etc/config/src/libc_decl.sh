cdf13acc1763b509f6da5f719f95e20029e13b4d
Martin Sebor <sebor@apache.org>
2006-10-10 00:03:37 +0000

    2006-10-09  Martin Sebor  <sebor@roguewave.com>
    	* libc_decl.sh (putenv, setenv, unsetenv): Added tests for POSIX
    	functions.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@454566 13f79535-47bb-0310-9956-ffa450edef68

--- etc/config/src/libc_decl.sh
+++ etc/config/src/libc_decl.sh
@@ -4,6 +4,26 @@
 #
 ##############################################################################
 #
+# Licensed to the Apache Software  Foundation (ASF) under one or more
+# contributor  license agreements.  See  the NOTICE  file distributed
+# with  this  work  for  additional information  regarding  copyright
+# ownership.   The ASF  licenses this  file to  you under  the Apache
+# License, Version  2.0 (the  "License"); you may  not use  this file
+# except in  compliance with the License.   You may obtain  a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the  License is distributed on an  "AS IS" BASIS,
+# WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+# implied.   See  the License  for  the  specific language  governing
+# permissions and limitations under the License.
+#
+# Copyright 2001-2006 Rogue Wave Software.
+#
+##############################################################################
+#
 # Test to determine which ANSI C library functions are declared in the
 # respective header or, if not, whether they are defined in the library
 # binary (libc or libm)
@@ -272,7 +292,7 @@ for h in $hdrs ; do
     stdlib)
         c90_funs="abort abs atexit atof atoi atol atoll bsearch calloc div exit free getenv labs ldiv llabs lldiv malloc mblen mbstowcs mbtowc qsort rand realloc srand strtod strtol strtoul system wcstombs wctomb"
         c99_funs="strtof strtold strtoll strtoull"
-        posix_funs="mkstemp"
+        posix_funs="mkstemp putenv setenv unsetenv"
 
         funs="$c90_funs $c99_funs $posix_funs"
         lib=c
