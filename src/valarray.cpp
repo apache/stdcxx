5d585dc6d8a3fdd9d40a23e00fd96d9802c29ec6
Martin Sebor <sebor@apache.org>
2006-12-11 23:32:25 +0000

    2006-12-11  Martin Sebor  <sebor@roguewave.com>
    	STDCXX-309
    	* valarray (operator[]): Invoked next_ind() at most once per each
    	iteration of the loop.
    	* valarray.cpp (next_ind): Returned start() instead of 0 after
    	wrapping around.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@485943 13f79535-47bb-0310-9956-ffa450edef68

--- src/valarray.cpp
+++ src/valarray.cpp
@@ -2,20 +2,27 @@
  *
  * valaray - Declarations for the Standard Library valarray
  *
- * $Id: //stdlib/dev/source/stdlib/valarray.cpp#15 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 1994-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -36,17 +43,19 @@ _RWSTD_SIZE_T gslice::next_ind ()
 	 
     while (__n && _C_r_length [__n - 1] == _C_length [__n - 1] - 1)
 	--__n;
-     
+
     if (0 == __n) {
         _C_reset    = true;
         _C_r_length = 0;
-        return 0;
-    }
-
-    if (_C_reset) { 
-        _C_reset = false;	  
         return _C_start;
     }
+     
+    if (_C_reset) {
+        _C_reset = false;
+        return _C_start;
+    }
+
+    _RWSTD_ASSERT (0 < __n);
 
     ++_C_r_length [__n - 1];
 
