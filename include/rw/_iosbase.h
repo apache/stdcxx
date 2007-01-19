095122c3cdf1de82d7308a421cd6f5ada22b94ec
Martin Sebor <sebor@apache.org>
2007-01-19 21:45:22 +0000

    2007-01-19  Martin Sebor  <sebor@roguewave.com>
    	STDCXX-326
    	* _iosbase.h (seek_dir, io_state, open_mode): Declared as int.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@497965 13f79535-47bb-0310-9956-ffa450edef68

--- include/rw/_iosbase.h
+++ include/rw/_iosbase.h
@@ -9,16 +9,23 @@
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 1994-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -201,10 +208,10 @@ struct _RWSTD_EXPORT ios_base: _RW::__rw_synchronized
 
 #ifndef _RWSTD_NO_DEPRECATED    
 
-    // D.6, p1
-    typedef seekdir          seek_dir;
-    typedef openmode         open_mode;
-    typedef iostate          io_state;
+    // D.6, p1 (must be integer types and not enums)
+    typedef int              seek_dir;
+    typedef int              open_mode;
+    typedef int              io_state;
     typedef _RWSTD_STREAMOFF streamoff;
     typedef _RWSTD_STREAMOFF streampos;
 
