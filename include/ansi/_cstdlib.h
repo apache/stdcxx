50b33c688af07dc418da0095857a331b1292cd03
Martin Sebor <sebor@apache.org>
2007-01-19 18:00:46 +0000

    2007-01-19  Martin Sebor  <sebor@roguewave.com>
    	STDCXX-323
    	* _cstdlib.h (abs, div): Added overloads for long.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@497889 13f79535-47bb-0310-9956-ffa450edef68

--- include/ansi/_cstdlib.h
+++ include/ansi/_cstdlib.h
@@ -3,20 +3,27 @@
  *
  * cstdlib - C++ Standard library interface to the ANSI C header stdlib.h
  *
- * $Id: //stdlib/dev/include/ansi/_cstdlib.h#9 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 2001-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -130,6 +137,18 @@ size_t wcstombs (char*, const wchar_t*, size_t);
     {
         return _RWSTD_C::qsort (__base, __n, __size,
                                _RWSTD_REINTERPRET_CAST (__rw_compar_t, __cmp));
+    }
+
+    inline long
+    abs (long __x)
+    {
+        return labs (__x);
+    }
+
+    inline ldiv_t
+    div (long __x, long __y)
+    {
+        return ldiv (__x, __y);
     }
 
     }   // extern "C++"
