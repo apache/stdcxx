1889725a66e78cad00dcebf92e33173e3d1345d0
Martin Sebor <sebor@apache.org>
2007-03-08 02:00:07 +0000

    2007-03-07  Martin Sebor  <sebor@roguewave.com>
    	STDCXX-349
    	* iostore.cpp (_C_copyfmt): Zeroed out a_size array to prevent
    	(bogus) gcc warnings about the variable being used uninitialized.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@515888 13f79535-47bb-0310-9956-ffa450edef68

--- src/iostore.cpp
+++ src/iostore.cpp
@@ -3,20 +3,27 @@
  * iostore.cpp - source of the C++ Standard Library 
  *               ios_base storage functions [lib.ios.base.storage]
  *
- * $Id: //stdlib/dev/source/stdlib/iostore.cpp#10 $
+ * $Id$
  *
  ***************************************************************************
  *
- * Copyright (c) 1994-2005 Quovadx,  Inc., acting through its  Rogue Wave
- * Software division. Licensed under the Apache License, Version 2.0 (the
- * "License");  you may  not use this file except  in compliance with the
- * License.    You    may   obtain   a   copy   of    the   License    at
- * http://www.apache.org/licenses/LICENSE-2.0.    Unless   required    by
- * applicable law  or agreed to  in writing,  software  distributed under
- * the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR
- * CONDITIONS OF  ANY KIND, either  express or implied.  See  the License
- * for the specific language governing permissions  and limitations under
- * the License.
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ * Copyright 1994-2006 Rogue Wave Software.
  * 
  **************************************************************************/
 
@@ -223,6 +230,11 @@ _C_copyfmt (const ios_base &rhs,
             memcpy (ia,  rusr->_C_iarray,  a_size [0] * sizeof *ia);
             memcpy (pa,  rusr->_C_parray,  a_size [1] * sizeof *pa);
             memcpy (cba, rusr->_C_cbarray, a_size [2] * sizeof *cba);
+        }
+        else {
+            // zero out array to prevent (bogus) gcc warnings
+            // about the variable being used uninitialized
+            memset (a_size, 0, sizeof a_size);
         }
 
         // copy the rest of rhs state (save for exceptions)
