1c9724312af3c5a43d47a0e53528c61febf75242
Farid Zaripov <faridz@apache.org>
2007-06-19 07:03:12 +0000

    2007-06-19 Farid Zaripov <Farid_Zaripov@epam.com>
    	* proclimits.h: New file with process limit functions
    	* NEW_THROWS.cpp: #included "proclimits.h"
    	* GLOBAL_BAD_ALLOC.cpp: Ditto
    	* STD_BAD_ALLOC.cpp: Ditto
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@548616 13f79535-47bb-0310-9956-ffa450edef68

--- etc/config/src/STD_BAD_ALLOC.cpp
+++ etc/config/src/STD_BAD_ALLOC.cpp
@@ -7,8 +7,11 @@
 #endif   // !_RWSTD_NO_NEW_THROWS && !_RWSTD_NO_NEW_OFLOW_SAFE
 
 #ifndef _RWSTD_NO_SETRLIMIT
-#  include <sys/resource.h>   // for setrlimit()
+// test for setrlimit() presence before compiling current file
 #endif
+
+// proclimits.h must be included only after #if[n]def _RWSTD_NO_SETRLIMIT
+#include "proclimits.h"
 
 #ifndef _RWSTD_NO_HONOR_STD
 #  ifdef _RWSTD_NO_STD_TERMINATE
@@ -81,12 +84,7 @@ int main ()
 
 #  else   // if defined (_RWSTD_NO_NEW_OFLOW_SAFE)
 
-#ifndef _RWSTD_NO_SETRLIMIT
-
-        struct rlimit rl = { 0, 0 };
-        setrlimit (RLIMIT_DATA, &rl);
-
-#endif   // _RWSTD_NO_SETRLIMIT
+        limit_memory (0);
 
         for (unsigned long n = 1UL << (sizeof (long) * 8 - 1);
              0 != n; n |= (n >> 1)) {
