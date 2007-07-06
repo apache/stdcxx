447782306110717e77039eaadec2da6152b29710
Martin Sebor <sebor@apache.org>
2007-07-06 19:59:39 +0000

    2007-07-06  Martin Sebor  <sebor@roguewave.com>
    	* mman.h: Header with declarations of portability helper functions
    	for the manipulation of memory mapped files.
    	* mman.cpp: Definitions of the said helpers.
    	* facet.cpp (__rw_mmap, __rw_unmmap): Moved functions to mman.cpp.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@554036 13f79535-47bb-0310-9956-ffa450edef68

--- src/facet.cpp
+++ src/facet.cpp
@@ -30,30 +30,14 @@
 
 #include <rw/_defs.h>
 
-// unistd.h is included here because of PR #26255
-#ifndef _MSC_VER
-#  include <unistd.h>
-#endif  // _MSC_VER
-
 #include <locale.h>   // for LC_XXX costants
 #include <stdlib.h>   // for bsearch(), getenv(), qsort()
 #include <string.h>   // for memcpy(), strlen()
 
-#include <sys/stat.h>
-
-#ifndef _MSC_VER
-#  include <sys/mman.h>
-#else
-#  include <windows.h>
-#  include <io.h>
-#endif   // _MSC_VER
-
-#include <sys/types.h>
-#include <fcntl.h>
-
 #include <iosfwd>         // for mbstate_t
 
 #include "access.h"
+#include "mman.h"
 
 #include <loc/_facet.h>
 #include <loc/_locale.h>
@@ -99,93 +83,6 @@ const char* __rw_get_cat_name (int category)
     }
 
     return 0;
-}
-
-
-// maps a named file into memory as shared, read-only, returns
-// the beginning address on success and fills `size' with the
-// size of the file; returns 0 on failure
-static
-void* __rw_mmap (const char* fname, _RWSTD_SIZE_T *size)
-{
-    _RWSTD_ASSERT (0 != fname);
-    _RWSTD_ASSERT (0 != size);
-
-#if !defined (_MSC_VER)
-    struct stat sb;
-    if (stat (fname, &sb) == -1)
-#else
-    struct _stat sb;
-    if (_stat (fname, &sb) == -1)
-#endif
-        return 0;
-
-    *size = sb.st_size;
-
-#if !defined(_MSC_VER)
-    const int fd = open (fname, O_RDONLY);
-
-    if (-1 == fd)
-        return 0;
-
-    // On HPUX systems MAP_SHARED will prevent a second mapping of the same
-    // file if the regions are overlapping; one solution is to make the 
-    // mapping private.
-#if defined(__hpux)
-    void *data = mmap (0, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-#else
-    void *data = mmap (0, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
-#endif // defined(__hpux__)
-
-    close (fd);
-
-#ifndef MAP_FAILED
-#  define MAP_FAILED (void*)-1
-#endif
-
-    if (MAP_FAILED == data)   // failure
-        return 0;
-#else
-    HANDLE mmf = 
-        CreateFile (fname, GENERIC_READ, FILE_SHARE_READ, NULL,
-                    OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-    if (mmf == INVALID_HANDLE_VALUE)
-        return 0;
-                              
-    HANDLE mmfv = 
-        CreateFileMapping (mmf, NULL, PAGE_READONLY, 0, 0, NULL);
-    if (mmfv == INVALID_HANDLE_VALUE) {
-        CloseHandle (mmf);
-        return 0;
-    }
-
-    void * data = 
-        MapViewOfFile (mmfv, FILE_MAP_READ, 0, 0, sb.st_size);
-
-    // The handles can be safely closed
-    CloseHandle (mmf);
-    CloseHandle (mmfv);
-
-#endif  // _MSC_VER
-
-    return data;
-}
-
-static inline 
-void __rw_munmap (const void* pcv, _RWSTD_SIZE_T size)
-{
-    _RWSTD_ASSERT (pcv && size);
-
-        // cast first to void*
-    void* pv = _RWSTD_CONST_CAST (void*, pcv);
-
-    // POSIX munmap() takes a void*, but not all platforms conform
-#ifndef _MSC_VER
-    munmap (_RWSTD_STATIC_CAST (_RWSTD_MUNMAP_ARG1_T, pv), size);
-#else
-    UnmapViewOfFile (pv);
-#endif  // _MSC_VER
 }
 
 
