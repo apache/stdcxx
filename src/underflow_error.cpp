a099d63269faf49549bfe5572272c3a45dab0545
Martin Sebor <sebor@apache.org>
2007-09-06 21:00:58 +0000

    2007-08-09  Martin Sebor  <sebor@roguewave.com>
    	* stdexcept (invalid_argument, length_error, underflow_error,
    	overflow_error, out_of_range): Declared virtual ctors to prevent
    	them from being implicitly defined by the compiler (and to reduce
    	the size of object files compiled from translation units that
    	#include the header). See also change 553643.
    	* invalid_argument.cpp: Defined virtual dtor.
    	* length_error.cpp: Same.
    	* underflow_error.cpp: Same.
    	* overflow_error.cpp: Same.
    	* out_of_range.cpp: Same.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@573376 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/underflow_error.cpp
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *
+ * underflow_error.cpp - definitions of class underflow_error members
+ *
+ * $Id$
+ *
+ ***************************************************************************
+ *
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ *
+ **************************************************************************/
+
+#define _RWSTD_LIB_SRC
+
+#include <stdexcept>
+#include <rw/_defs.h>
+
+
+_RWSTD_NAMESPACE (std) {
+
+// outlined to avoid generating a vtable in each translation unit
+// that uses the class
+/* virtual */ underflow_error::
+~underflow_error () _THROWS (())
+{
+    // no-op
+}
+
+}   // namespace std
