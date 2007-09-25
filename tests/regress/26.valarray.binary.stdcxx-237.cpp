c2bb7450f0fdc786629bf4476208f1ac6424a017
Martin Sebor <sebor@apache.org>
2007-09-25 00:34:07 +0000

    2007-09-25  Martin Sebor  <sebor@roguewave.com>
    	* 26.valarray.binary.stdcxx-237.cpp: Added regression test for STCXX-237.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stdcxx/trunk@579026 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/regress/26.valarray.binary.stdcxx-237.cpp
@@ -0,0 +1,42 @@
+/************************************************************************
+ *
+ * 26.valarray.binary.stdcxx-237.cpp - regression test for STDCXX-237
+ *
+ *   http://issues.apache.org/jira/browse/STDCXX-237
+ *
+ * $Id$
+ *
+ ***************************************************************************
+ *
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ * 
+ **************************************************************************/
+
+#include <cassert>
+#include <valarray>
+
+int main()
+{
+    std::valarray<int> ia;
+    std::gslice        gs;
+
+    std::valarray<int> res = std::operator%<int>(ia [gs], ia [gs]);
+
+    assert (0 == res.size ());
+
+    return 0;
+} 
