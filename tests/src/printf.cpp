635d4372be9fd304d69494b2d955661f3e7c62fd
Farid Zaripov <faridz@apache.org>
2008-03-15 11:25:40 +0000

    2008-03-15 Farid Zaripov <farid_zaripov@epam.com>
    	* tests/src/printf.cpp (rw_vasnprintf): Add assertion when processing
    	the "%{+}" directive to detect buffer overruns.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/stdcxx/trunk@637394 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/printf.cpp
+++ tests/src/printf.cpp
@@ -977,6 +977,7 @@ rw_vasnprintf (char **pbuf, size_t *pbufsize, const char *fmt, va_list varg)
         // over it
         fmt        += 4;
         buf.endoff  = *buf.pbuf ? strlen (*buf.pbuf) : 0;
+        RW_ASSERT (buf.endoff < *buf.pbufsize);
     }
     else if (*buf.pbuf)
         **buf.pbuf = '\0';
