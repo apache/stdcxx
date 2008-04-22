3a0db177013551ff84a3c7cf9850e63d9299aed4
Martin Sebor <sebor@apache.org>
2008-04-22 02:37:29 +0000

    2008-04-21  Martin Sebor  <sebor@roguewave.com>
    	* tests/regress/27.stringbuf.overflow.stdcxx-149.cpp: Added
    	a regression test for STDCXX-149.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/stdcxx/trunk@650360 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/regress/27.stringbuf.overflow.stdcxx-149.cpp
@@ -0,0 +1,55 @@
+/************************************************************************
+ *
+ * 27.stringbuf.overflow.stdcxx-149.cpp - regression test for STDCXX-149
+ *
+ * http://issues.apache.org/jira/browse/STDCXX-149
+ *
+ * $Id$
+ *
+ ***************************************************************************
+ *
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ * 
+ **************************************************************************/
+
+#include <cassert>
+#include <sstream>
+
+int main ()
+{
+    // Exercise the implementation of the resolution of LWG issue 432:
+    // http://www.open-std.org/jtc1/sc22/wg21/docs/lwg-defects.html#432
+
+    struct: std::stringbuf {
+        using std::stringbuf::eback;
+        using std::stringbuf::egptr;
+        using std::stringbuf::pbase;
+        using std::stringbuf::pptr;
+        using std::stringbuf::epptr;
+        using std::stringbuf::overflow;
+    } buf;
+
+    buf.overflow ('x');
+
+    const int write_positions = buf.epptr () - buf.pbase ();
+
+    const int pass = 1 < write_positions;
+
+    assert (pass);
+
+    return 1 - pass;
+}
