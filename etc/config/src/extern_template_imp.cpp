ddfd83642b9d09e949ab506045c20380a6536d59
Farid Zaripov <faridz@apache.org>
2008-07-01 15:44:50 +0000

    2008-07-01  Farid Zaripov  <faridz@apache.com>
    	STDCXX-339
    	* etc/config/src/EXTERN_MEMBER_TEMPLATE.cpp: Removed dependency
    	on deleted config test. Corrected name of the config macro
    	_RWSTD_NO_MEMBER_TEMPLATES. Replaced NO_MEMBER_TEMPLATE to
    	NO_MEMBER_TEMPLATES for consistency.
    	* etc/config/src/extern_template_imp.cpp: Ditto.
    	* etc/config/src/extern_template_imp.h: Removed using of the
    	NO_INLINE_MEMBER_TEMPLATE macro. Replaced NO_MEMBER_TEMPLATE to
    	NO_MEMBER_TEMPLATES for consistency.
    	* etc/config/src/MEMBER_TEMPLATES.cpp: Removed inclusion of the
    	deleted config test.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/stdcxx/branches/4.2.x@673109 13f79535-47bb-0310-9956-ffa450edef68

--- etc/config/src/extern_template_imp.cpp
+++ etc/config/src/extern_template_imp.cpp
@@ -26,13 +26,9 @@
 // establish dependencies on the config tests and define config
 // macros used in the header below (the are not autodetected
 // in headers)
-#ifdef _RWSTD_NO_INLINE_MEMBER_TEMPLATE
-#  define NO_INLINE_MEMBER_TEMPLATE
-#endif   // _RWSTD_NO_INLINE_MEMBER_TEMPLATE
-
-#ifndef _RWSTD_NO_MEMBER_TEMPLATE
-#  define NO_MEMBER_TEMPLATE
-#endif   // _RWSTD_NO_MEMBER_TEMPLATE
+#ifndef _RWSTD_NO_MEMBER_TEMPLATES
+#  define NO_MEMBER_TEMPLATES
+#endif   // _RWSTD_NO_MEMBER_TEMPLATES
 
 // explicitly instantiate the template defined in the header
 #define INSTANTIATE
