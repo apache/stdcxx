ddf0085e139bb7bb6cabf15ad1616f19fc859145
Farid Zaripov <faridz@apache.org>
2008-09-08 16:52:13 +0000

    2008-09-08  Farid Zaripov  <faridz@apache.org>
    	Merged revs 681328, 693158 from 4.2.x.
    
    	2008-07-31  Farid Zaripov  <faridz@apache.org>
    
    	* tests/regress/25.uninitialized.copy.stdcxx-976.cpp: New regression
    	test for STDCXX-976 issue.
    
    	2008-09-08  Farid Zaripov  <faridz@apache.org>
    
    	* tests/regress/21.string.stdcxx-1009.cpp: New regression
    	test for STDCXX-1009 issue.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/stdcxx/trunk@693160 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/regress/25.uninitialized.copy.stdcxx-976.cpp
@@ -0,0 +1,72 @@
+/************************************************************************
+ *
+ * 25.uninitialized.copy.stdcxx-976.cpp - regression test for STDCXX-976
+ *
+ * http://issues.apache.org/jira/browse/STDCXX-976
+ *
+ * $Id$
+ *
+ ***************************************************************************
+ *
+ * Licensed to the Apache Software  Foundation (ASF) under one or more
+ * contributor  license agreements.  See  the NOTICE  file distributed
+ * with  this  work  for  additional information  regarding  copyright
+ * ownership.   The ASF  licenses this  file to  you under  the Apache
+ * License, Version  2.0 (the  "License"); you may  not use  this file
+ * except in  compliance with the License.   You may obtain  a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the  License is distributed on an  "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY  KIND, either  express or
+ * implied.   See  the License  for  the  specific language  governing
+ * permissions and limitations under the License.
+ * 
+ **************************************************************************/
+
+#include <memory>
+#include <iterator>
+
+
+template <class T>
+struct InputIterator: std::iterator<std::input_iterator_tag, T>
+{
+    T *p_;
+
+    InputIterator (T *p): p_ (p) { }
+    InputIterator (const InputIterator &rhs): p_ (rhs.p_) { }
+
+    InputIterator& operator= (const InputIterator &rhs)
+    { p_ = rhs.p_; return *this; }
+
+    T operator* () const { return *p_; }
+    
+    InputIterator& operator++ () { return ++p_, *this; }
+    InputIterator operator++ (int) {
+        return ++p_, InputIterator (p_ - 1);
+    }
+
+    bool operator== (const InputIterator &rhs) const { return p_ == rhs.p_; }
+    bool operator!= (const InputIterator &rhs) const { return p_ != rhs.p_; }
+};
+
+
+int main ()
+{
+    typedef InputIterator<char>                Iter;
+    typedef InputIterator<const char>          CIter;
+    typedef InputIterator<volatile char>       VIter;
+    typedef InputIterator<const volatile char> CVIter;
+
+    char src [5] = "abcd";
+    char dst [5];
+
+    std::uninitialized_copy (Iter (src),   Iter (src + 5),   dst);
+    std::uninitialized_copy (CIter (src),  CIter (src + 5),  dst);
+    std::uninitialized_copy (VIter (src),  VIter (src + 5),  dst);
+    std::uninitialized_copy (CVIter (src), CVIter (src + 5), dst);
+
+    return 0;
+}
