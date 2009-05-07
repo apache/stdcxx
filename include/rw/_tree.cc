apache/stdcxx

commit 68205fae2de5aa0d1318df80d6187b2fb6f11435

2009-01-03  Martin Sebor  <sebor@roguewave.com>
	* include/rw/_tree.cc [_RWSTDDEBUG](__rb_tree::insert,
	__rb_tree::erase): Rewrote a for loop to silence gcc warning:
	suggest a space before ‘;’ or explicit braces around empty body
	in ‘for’ statement.
	* include/algorithm (find, find_if): Same.
	* include/algorithm.cc (__unguarded_partition, next_permutation,
	prev_permutation): Rewrote a while loop to silence said gcc
	warning.


git-svn-id: https://svn.apache.org/repos/asf/stdcxx/branches/4.2.x@731136 13f79535-47bb-0310-9956-ffa450edef68

include/rw/_tree.cc
@@ -338,7 +338,8 @@ insert (iterator __it, const value_type &__v, bool __dup)
 
     {   // verify the consistency of the tree
         size_type __two_logN = 0;
-        for (size_type __i = size () + 1; __i >>= 1; ++__two_logN);
+        for (size_type __i = size () + 1; __i >>= 1; )
+            ++__two_logN;
 
         __two_logN *= 2;
 
@@ -408,7 +409,8 @@ __rb_tree<_Key, _Val, _KeyOf, _Comp, _Alloc>::erase (iterator __it)
 
     {   // verify the consistency of the tree
         size_type __two_logN = 0;
-        for (size_type __i = size () + 1; __i >>= 1; ++__two_logN);
+        for (size_type __i = size () + 1; __i >>= 1; )
+            ++__two_logN;
 
         __two_logN *= 2;
 
@@ -844,7 +846,8 @@ erase (iterator __first, iterator __last)
         // return end()
         __tmp = end ();
     } else
-        for (__tmp = end (); !(__first == __last); __tmp = erase (__first++));
+        for (__tmp = end (); !(__first == __last); __tmp = erase (__first))
+            ++__first;
 
     return __tmp;
 }

include/algorithm
@@ -93,7 +93,8 @@ find (_InputIter __first, _InputIter __last, const _TypeT &__val)
 {
     _RWSTD_ASSERT_RANGE (__first, __last);
 
-    for (; !(__first == __last) && !(*__first == __val); ++__first);
+    while (!(__first == __last) && !(*__first == __val))
+        ++__first;
 
     return __first;
 }
@@ -105,7 +106,8 @@ find_if (_InputIter __first, _InputIter __last, _Predicate __pred)
 {
     _RWSTD_ASSERT_RANGE (__first, __last);
 
-    for (; !(__first == __last) && __pred (*__first) == false; ++__first);
+    while (!(__first == __last) && __pred (*__first) == false)
+        ++__first;
 
     return __first;
 }

include/algorithm.cc
@@ -791,9 +791,10 @@ _RandomAccessIter __unguarded_partition (_RandomAccessIter __first,
     _RWSTD_ASSERT_RANGE (__first, __last);
 
     for ( ; ; ++__first) {
-        for (; __comp (*__first, __pivot); ++__first);
+        while (__comp (*__first, __pivot))
+            ++__first;
 
-        while (__comp (__pivot, *--__last));
+        while (__comp (__pivot, *--__last)) { /* no-op */ }
 
         if (!(__first < __last))
             return __first;
@@ -1826,7 +1827,7 @@ bool next_permutation (_BidirIter __first, _BidirIter __last, _Compare __comp)
 
             _BidirIter __j = __last;
 
-            while (__comp (*__i, *--__j) == false);
+            while (__comp (*__i, *--__j) == false) { /* no-op */ }
 
             _STD::iter_swap (__i, __j);
 
@@ -1870,7 +1871,7 @@ bool prev_permutation (_BidirIter __first, _BidirIter __last, _Compare __comp)
 
             _BidirIter __j = __last;
 
-            while (__comp (*--__j, *__i) == false);
+            while (__comp (*--__j, *__i) == false) { /* no-op */ }
 
             _STD::iter_swap (__i, __j);
 

commit 3104b7890b63dae378ce9d4c541271090f2c728e

2009-05-07  Farid Zaripov  <faridz@apache.org>
	* include/rw/_tree.cc (erase): Fixed bug introduced in r731136.


git-svn-id: https://svn.apache.org/repos/asf/stdcxx/branches/4.2.x@772730 13f79535-47bb-0310-9956-ffa450edef68

include/rw/_tree.cc
@@ -846,8 +846,8 @@ erase (iterator __first, iterator __last)
         // return end()
         __tmp = end ();
     } else
-        for (__tmp = end (); !(__first == __last); __tmp = erase (__first))
-            ++__first;
+        for (__tmp = end (); !(__first == __last); )
+            __tmp = erase (__first++);
 
     return __tmp;
 }