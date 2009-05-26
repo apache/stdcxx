2fae1fa227bf19729234ce4562ab1eb0924c6cf9
Martin Sebor <sebor@apache.org>
2009-05-26 15:59:17 +0000

    2009-05-26  Martin Sebor  <sebor@apache.org>
    	STDCXX-1036
    	* src/iosdata.h (ios_base::_C_usr_data::_C_alloc,
    	ios_base::_C_usr_data::_C_dealloc): Moved functions from here...
    	* src/iostore.cpp: ...to here and outlined to silence gcc 4.4 -Winline
    	warnings.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/stdcxx/branches/4.2.x@778780 13f79535-47bb-0310-9956-ffa450edef68

--- src/iosdata.h
+++ src/iosdata.h
@@ -22,15 +22,13 @@
  * implied.   See  the License  for  the  specific language  governing
  * permissions and limitations under the License.
  *
- * Copyright 1994-2006 Rogue Wave Software.
+ * Copyright 1994-2006 Rogue Wave Software, Inc.
  * 
  **************************************************************************/
 
 #ifndef _RWSTD_IOSDATA_H_INCLUDED
 #define _RWSTD_IOSDATA_H_INCLUDED
 
-
-#include <string.h>
 
 #include <rw/_iosbase.h>
 #include <rw/_defs.h>
@@ -68,66 +66,6 @@ struct ios_base::_C_usr_data
     static _C_usr_data _C_std_usr_data [2];
 };
 
-
-inline /* static */ ios_base::_C_usr_data*
-ios_base::_C_usr_data::_C_alloc (_C_fire_fun pfire)
-{
-    _TRY {
-        // rely on 0-initialization of PODs
-        _C_usr_data* const pdata = new _C_usr_data ();
-
-#ifdef _RWSTD_NO_NEW_THROWS
-
-        if (!pdata)
-            return 0;
-
-#endif   // _RWSTD_NO_NEW_THROWS
-
-        _RWSTD_ASSERT (0 != pdata);
-
-#ifndef _RWSTD_NO_POD_ZERO_INIT
-
-        // assert that the POD ctor above zeroed out all members
-        _RWSTD_ASSERT (!pdata->_C_tie);
-        _RWSTD_ASSERT (!pdata->_C_iarray);
-        _RWSTD_ASSERT (!pdata->_C_parray);
-        _RWSTD_ASSERT (!pdata->_C_cbarray);
-        _RWSTD_ASSERT (!pdata->_C_isize);
-        _RWSTD_ASSERT (!pdata->_C_psize);
-        _RWSTD_ASSERT (!pdata->_C_cbsize);
-
-#else   // if defined (_RWSTD_NO_POD_ZERO_INIT)
-
-        memset (pdata, 0, sizeof *pdata);
-
-#endif   // _RWSTD_NO_POD_ZERO_INIT
-
-        pdata->_C_fire = pfire;
-
-        return pdata;
-    }
-    _CATCH (...) {
-        return 0;
-    }
-}
-
-
-inline /* static */ void
-ios_base::_C_usr_data::_C_dealloc (_C_usr_data *ptr)
-{
-    if (ptr) {
-        operator delete (ptr->_C_iarray);
-        operator delete (ptr->_C_parray);
-        operator delete (ptr->_C_cbarray);
-
-        if (   ptr != _C_usr_data::_C_std_usr_data
-            && ptr != _C_usr_data::_C_std_usr_data + 1)
-            delete ptr;
-    }
-}
-
-
 }   // namespace std
-
 
 #endif   // _RWSTD_IOSDATA_H_INCLUDED
