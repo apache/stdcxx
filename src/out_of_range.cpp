929a978543cfee682aafbd9b2b143148bd2e0148
Farid Zaripov <faridz@apache.org>
2009-08-27 10:37:20 +0000

    2009-08-27  Farid Zaripov  <faridz@apache.org>
    	* etc/config/src/DOMAIN_ERROR_DTOR.cpp: Reverted changed from r777606.
    	* etc/config/src/INVALID_ARGUMENT_DTOR.cpp: Same.
    	* etc/config/src/LENGTH_ERROR_DTOR.cpp: Same.
    	* etc/config/src/LOGIC_ERROR_DTOR.cpp: Same.
    	* etc/config/src/OUT_OF_RANGE_DTOR.cpp: Same.
    	* etc/config/src/OVERFLOW_ERROR_DTOR.cpp: Same.
    	* etc/config/src/RANGE_ERROR_DTOR.cpp: Same.
    	* etc/config/src/RUNTIME_ERROR_DTOR.cpp: Same.
    	* etc/config/src/UNDERFLOW_ERROR_DTOR.cpp: Same.
    	* src/domain_error.cpp: Same.
    	* src/invalid_argument.cpp: Same.
    	* src/length_error.cpp: Same.
    	* src/logic_error.cpp: Same.
    	* src/out_of_range.cpp: Same.
    	* src/overflow_error.cpp: Same.
    	* src/range_error.cpp: Same.
    	* src/runtime_error.cpp: Same.
    	* src/underflow_error.cpp: Same.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/stdcxx/branches/4.3.x@808357 13f79535-47bb-0310-9956-ffa450edef68

--- src/out_of_range.cpp
+++ src/out_of_range.cpp
@@ -32,8 +32,6 @@
 
 _RWSTD_NAMESPACE (std) {
 
-#ifdef _RWSTD_NO_OUT_OF_RANGE_DTOR
-
 // outlined to avoid generating a vtable in each translation unit
 // that uses the class
 /* virtual */ out_of_range::
@@ -41,7 +39,5 @@ _RWSTD_NAMESPACE (std) {
 {
     // no-op
 }
-
-#endif   // _RWSTD_NO_OUT_OF_RANGE_DTOR
 
 }   // namespace std
